1c3c1212cef5de39b28bb7980e4d3b9f91b4188b
minjyun3011 <grand3canyon10@gmail.com>
2024-02-10 09:03:17 +0900

    DBモデル定義からテーブル作成まで

--- env.py
+++ env.py
@@ -1,2 +1,10 @@
 import os
 print(os.environ.get("PWD"))
+print(os.environ.get("SECRET"))
+
+from dotenv import load_dotenv
+load_dotenv()
+print(os.environ["SECRET"])  # 存在しなかった場合はKeyError
+print(os.environ.get("SECRET"))  # 存在しなかった場合はNone
+print(os.environ.get("SECRET", "PUBLIC"))  # 存在しなかった場合は"PUBLIC"
+
